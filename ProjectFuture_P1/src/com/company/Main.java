55a83eba21108b4917efa5db5fb1094dc5a1c707
DimitrisAretis <mitsar20@gmail.com>
2018-11-29 00:04:06 +0200

    class pou kanei connect me ti vasi DB + kodikas sti main pou kalei tin class, stelnei query kai tuponei ta apotelesmata

--- ProjectFuture_P1/src/com/company/Main.java
+++ ProjectFuture_P1/src/com/company/Main.java
@@ -1,14 +1,38 @@
 package com.company;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main
 {
 
     public static void main(String[] args)
     {
-        Utils.Menu.display();
+        //Utils.Menu.display();
 
         //Uncomment these lines to check the functionality of the methods
         //System.out.println(Utils.DaysToDate(10));
         //Utils.PatternCheck("PKA-5139");
+
+        String entoli = "SELECT ID, first_name, last_name FROM owner";
+
+        try (Connection conn = MYSQLConnector.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(entoli))
+        {
+            while (rs.next())
+            {
+                System.out.println(rs.getString("ID") + "\t" +
+                                   rs.getString("first_name") + "\t" +
+                                   rs.getString("last_name"));
+
+            }
+        }
+        catch (SQLException ex)
+        {
+            System.out.println(ex.getMessage());
+        }
     }
 }
