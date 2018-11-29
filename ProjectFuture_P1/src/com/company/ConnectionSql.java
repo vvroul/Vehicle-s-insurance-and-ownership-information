eb30c114b5575c05be2d96c581ed5c2ca38fb248
Vasileios Vrouliotis <vvrouliotis@gmail.com>
2018-11-30 00:35:11 +0200

    Cleaned code - Going for final implementation

--- ProjectFuture_P1/src/com/company/ConnectionSql.java
+++ ProjectFuture_P1/src/com/company/ConnectionSql.java
@@ -1,62 +1,93 @@
 package com.company;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.*;
 import java.util.Date;
+import java.util.Properties;
 
-public class ConnectionSql {
+class ConnectionSql {
 
-    public static final String USERNAME = "root";
-    public static final String PASSWORD = "Iadorec#741852963";
-    public static final String CONN_STRING = "jdbc:mysql://127.0.0.1:3306/";
-    public static final String DB_NAME = "team8db";
+    private static Connection connection;
 
-    public static Connection connection;
-
-    public static boolean open(){
-        try{
-            connection = DriverManager.getConnection(CONN_STRING+DB_NAME+"?useSSL=false",USERNAME,PASSWORD);
-            System.out.println("Connected");
-            return true;
-
-        }catch (SQLException e){
-            System.out.println("Could not connected" +e.getMessage());
+    //Open database connection
+    static boolean open()
+    {
+        try (FileInputStream fileInputStream = new FileInputStream("StoixeiaVasis.properties"))
+        {
+            Properties props = new Properties();
+            props.load(fileInputStream);
+            String url = props.getProperty("url");
+            String user = props.getProperty("user");
+            String password = props.getProperty("password");
+            connection = DriverManager.getConnection(url, user, password);
+        }
+        catch (IOException IOex)
+        {
+            System.out.println(IOex.getMessage());
             return false;
         }
+        catch (SQLException SQLex)
+        {
+            System.out.println("SQL exception occurred. Couldn't connect to database.");
+            return false;
+        }
+        finally
+        {
+            System.out.println("Connected");
+        }
+        return true;
     }
 
-    public static void close() {
-        try {
-            if(connection!=null) {
+
+    //Close database connection
+    static void close()
+    {
+        try
+        {
+            if(connection!=null)
+            {
                 connection.close();
             }
-        } catch (SQLException e) {
+        }
+        catch (SQLException e)
+        {
             System.out.println("Could not close connection"+e.getMessage());
         }
-
     }
 
-    public static void queryPlate(String input){
 
-        if(connection==null){
+    //Query that returns the insurance status of a given query
+    static void queryPlate(String input)
+    {
+        if(connection==null)
+        {
             System.out.println("There's no connection to a database");
             return;
         }
         try(Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from vehicle where plate = "+"'"+input+"'")){
-            if(resultSet.next()) {
+            ResultSet resultSet = statement.executeQuery("select * from vehicle where plate = "+"'"+input+"'"))
+        {
+            if(resultSet.next())
+            {
                 Date now = new Date();
                 Date date = resultSet.getDate("ins_date_end");
-                if(date==null){
+                if(date==null)
+                {
                     System.out.println("The vehicle has no insurance");
                     return;
                 }
                 System.out.println("The vehicle with plate number : "+resultSet.getString("plate") +
                         " " + (now.after(date)?"has an expired insurance, and the insurance expired at ":
                         "has a valid insurance, and the insurance expires at ")+date);
-            }else{
+            }
+            else
+            {
                 System.out.println("No vehicle has such plate");
             }
-        }catch (SQLException e) {
+        }
+        catch (SQLException e)
+        {
             System.out.println("something went wrong" +e.getMessage());
             e.printStackTrace();
         }
--- ProjectFuture_P1/src/com/company/Main.java
+++ ProjectFuture_P1/src/com/company/Main.java
@@ -12,24 +12,6 @@ public class Main
     public static void main(String[] args)
     {
         Utils.Menu.display();
-        /*String entoli = "SELECT ID, first_name, last_name FROM owner";
-
-        try (Connection conn = MYSQLConnector.getConnection();
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(entoli))
-        {
-            while (rs.next())
-            {
-                System.out.println(rs.getString("ID") + "\t" +
-                        rs.getString("first_name") + "\t" +
-                        rs.getString("last_name"));
-
-            }
-        }
-        catch (SQLException ex)
-        {
-            System.out.println(ex.getMessage());
-        }*/
     }
 }
 
